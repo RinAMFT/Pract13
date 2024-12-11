96d3fea4ae38d9f8d040ef46c2cbf0f4bbf1781d
RinAMFT <mucurkaarina@gmail.com>
2024-12-11 09:16:11 +0200

    Add files via upload

---
+++ ConsoleApp23/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp23
+{
+    internal class Program
+    {
+        /// <summary>
+        /// По номеру месяца вывести название времени года.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            byte a = Input("month=");
+            Console.WriteLine($"Время года:{GetDay(a)}");
+            Console.ReadKey();
+        }
+        static byte Input(string message)
+        {
+            Console.Write(message);
+            return byte.Parse(Console.ReadLine());
+        }
+        static string GetDay(int month)
+        {
+            switch (month)
+            {
+                case 1: return "Зима.";
+                case 2: return "Зима.";
+                case 3: return "Весна.";
+                case 4: return "Весна.";
+                case 5: return "Весна.";
+                case 6: return "Лето.";
+                case 7: return "Лето.";
+                case 8: return "Лето.";
+                case 9: return "Осень.";
+                case 10: return "Осень.";
+                case 11: return "Осень.";
+                case 12: return "Зима.";
+                default: return "Ошибка";
+
+
+            }
+        }
+    }
+}
