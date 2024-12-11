d020cbfd9af1deb65e59073e4f1fa8f09f0614c3
RinAMFT <mucurkaarina@gmail.com>
2024-12-11 09:43:47 +0200

    Add files via upload

---
+++ ConsoleApp24/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите первую букву: ");
+            char letter = Char.ToLower(Console.ReadKey().KeyChar);
+            Console.WriteLine();
+
+            switch (letter)
+            {
+                case 'ф':
+                    Console.WriteLine("Физика");
+                    break;
+                case 'м':
+                    Console.WriteLine("Математика");
+                    break;
+                case 'и':
+                    Console.WriteLine("История");
+                    break;
+                case 'г':
+                    Console.WriteLine("География");
+                    break;
+                case 'б':
+                    Console.WriteLine("Биология");
+                    break;
+                default:
+                    Console.WriteLine("Неизвестная буква");
+                    break;
+            }
+            Console.ReadKey();
+        }
+
+    }
+
+
+}
